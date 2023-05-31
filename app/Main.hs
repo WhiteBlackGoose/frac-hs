44611351f850d685fe9eec3a0fed048bdb3c2211
WhiteBlackGoose <wbg@angouri.org>
2023-05-31 16:19:37 +0200

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -6,22 +6,25 @@ import Data.Vector.Unboxed.Base (Vector)
 import Data.Word (Word8)
 import Data.Vector.Unboxed (generate)
 
-type Criterion a = (RealFloat a, Ord a) => (Complex a -> Complex a) -> Complex a -> Bool
+type Color = (Word8, Word8, Word8, Word8)
+
+type Criterion a = (RealFloat a, Ord a) => (Complex a -> Complex a) -> Complex a -> Color
 
 belongs :: Criterion a
 belongs = 
   let 
+    maxPrec = 100
     belongs prec seq c
-      | magnitude c >= 2.0 = False
-      | prec == 0 = True
+      | magnitude c >= 2.0 = (255, (255 * prec) `div` maxPrec, 0, 128)
+      | prec == 0 = (255, 0, 128, 0)
       | otherwise = belongs (prec-1) seq (seq c)
   in
-    belongs 100
+    belongs maxPrec
 
 mandelbrot :: RealFloat a => Complex a -> Complex a -> Complex a
 mandelbrot c z = z ^ 2 + c
 
-render :: (Criterion a) -> (Int, Int) -> (Float, Float, Float, Float) -> ImageData
+render :: (Complex Float -> Complex Float -> Complex Float) -> (Int, Int) -> (Float, Float, Float, Float) -> ImageData
 render crit (w, h) (rx, ry, rw, rh) = 
   let
     -- vec :: Vector Word8 = generate (w * h * 4) (\i -> fromIntegral( i `mod` h `div` w) :: Word8)
@@ -31,12 +34,18 @@ render crit (w, h) (rx, ry, rw, rh) =
         thX :: Float = fromIntegral (i `div` 4 `div` h) / (fromIntegral w) * rw + rx
         thY :: Float = fromIntegral (i `div` 4 `mod` h) / (fromIntegral h) * rh + ry
         c = thX :+ thY
+        (a, r, g, b) = belongs (crit c) 0
       in
-        if belongs (mandelbrot c) 0 then 128 else 0)
+        case i `mod` 4 of
+          0 -> a
+          1 -> r
+          2 -> g
+          3 -> b
+        )
   in
     ImageData w h vec
 
 
 main = blankCanvas 3000 $ \ context -> do -- start blank canvas on port 3000
   send context $ do                       -- send commands to this specific context
-    putImageData ((render (\_ _ -> True) (300, 300) (-2.0, -2.0, 4.0, 4.0)), [0.0, 0.0])
+    putImageData ((render mandelbrot (400, 400) (-1.5, -1.1, 2.2, 2.2)), [0.0, 0.0])
