631def56cf819f4baf9b9768767f3f8146a1b922
WhiteBlackGoose <wbg@angouri.org>
2023-06-01 17:04:14 +0200

    perf improved

--- app/Fractals.hs
+++ app/Fractals.hs
@@ -5,23 +5,19 @@ module Fractals (
 ) where
 import Data.Complex (Complex, magnitude)
 
-type Criterion a = (RealFloat a, Ord a) => (Complex a -> Complex a) -> Complex a -> (Int, Int)
-
 type Fractal a = Complex a -> (Int, Int)
 
 -- https://en.wikipedia.org/wiki/Mandelbrot_set
-mandelbrot :: RealFloat a => Fractal a
+mandelbrot :: Fractal Double
 mandelbrot c = belongs (\z -> z ** 2 + c) 0
 
 -- https://en.wikipedia.org/wiki/Julia_set
-julia :: RealFloat a => Complex a -> Fractal a
+julia :: Complex Double -> Fractal Double
 julia p = belongs (\z -> z ** 2 + p)
 
-belongs :: forall a. Criterion a
-belongs (crit :: Complex a -> Complex a) (co :: Complex a) =
-  let
+belongs :: (Complex Double -> Complex Double) -> Complex Double -> (Int, Int)
+belongs crit co =
+    (maxPrec, length (take maxPrec z))
+  where
     maxPrec :: Int = 80
-    z = takeWhile ((<=2.0) . magnitude) (iterate crit co :: [Complex a])
-    n = [0..maxPrec]
-  in
-    (maxPrec, snd . last $ zip z n)
+    z = takeWhile ((<=2.0) . magnitude) (iterate crit co)
